981ad78bb1f86de7210d0d266fe31af5b7696e2f
Felipe Coelho <felipecoelho.ness@gmail.com>
2024-01-23 01:48:58 -0300

    feat: create title and head

--- src/pages/Main/Burgers/index.tsx
+++ src/pages/Main/Burgers/index.tsx
@@ -1,3 +1,34 @@
+import { Head } from '../../../components/Head'
+import { SnackTitle } from '../../../components/SnackTitle'
+import { Snacks } from '../../../components/Snacks'
+
 export default function Burgers() {
-  return <h1>Burgers</h1>
+  const data = [
+    {
+      id: 1,
+      snack: 'burger',
+      name: 'Mega',
+      description:
+        'O artesanal mais pedido da casa, com três carnes, queijo, bacon e molho especial',
+      price: 29.99,
+      image: 'https://i.imgur.com/upjIUnG.jpg',
+    },
+    {
+      id: 2,
+      snack: 'burger',
+      name: 'Extra Bacon',
+      description:
+        'O artesanal para amantes de bacon, com duas carnes, queijo, bacon e molho especial',
+      price: 24.99,
+      image: 'https://i.imgur.com/B4J04AJ.jpg',
+    },
+  ]
+
+  return (
+    <>
+      <Head title='Hambúrgueres' description='Nossos melhores burguers' />
+      <SnackTitle>Hambúrgueres</SnackTitle>
+      <Snacks snacks={data} />
+    </>
+  )
 }
